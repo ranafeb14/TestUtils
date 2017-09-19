6c8a08a325e723afe810b7137ab0904c4fd336fd
Rana Prathap Bhoosi <rana.bhoosi@imaginea.com>
2017-09-19 11:05:42 +0000

    Test New files added - [appMenuItems,Leave_Application_DB,MyJavaService,TestCorridor,TestDB,securityService,wm_textarea,data,momentLocale,mongodb-driver-3.2.2.jar,app.properties,Readme.txt,momentLocale] Files modified - [Main,WEB-INF,profiles,.gitignore,.wmproject.properties,pom.xml,index.html,pages-config.json,en.json]

---
+++ src/main/webapp/WEB-INF/prefabs/wm_textarea/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "wm_textarea",
+  "homePage" : "Main",
+  "name" : "wm_textarea",
+  "platformType" : "DEFAULT",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.03"
+};
